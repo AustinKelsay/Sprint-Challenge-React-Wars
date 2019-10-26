caadd0edd5ece29cd6b458a2acb9c3020504457a
AustinKelsay <austinkelsay@yahoo.com>
2019-10-26 11:16:36 -0500

    Fetched the SWAPI using axios and .get

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
 import './App.css';
 
 const App = () => {
@@ -9,9 +10,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    axios
+    .get('https://swapi.co/api/people/')
+    .then((res) => {
+        console.log(res);
+        setState(res.results)
+    })
+    .catch((err) => {
+        console.log("ERROR" + err);
+    })
+}, []);
+if (!state) return <h3>Loading...</h3>;
+
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <h1 className="Header">{state}</h1>
     </div>
   );
 }
